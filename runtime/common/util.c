62664631506971a750caec08fdeab7c9eed123ef
Anatoly Trosinenko <anatoly.trosinenko@gmail.com>
2021-10-24 20:35:53 +0300

    common: Print relevant environment variables at startup

--- runtime/common/util.c
+++ runtime/common/util.c
@@ -135,26 +135,33 @@ void inhibit_syscalls(struct fuzzer_state *state, bool inhibited) {
   state->mutable_state.syscalls_inhibited = inhibited;
 }
 
+static const char *get_config(const char *name) {
+  const char *result = getenv(name);
+  const char *printed = result ? result : "<default>";
+  fprintf(stderr, "config: %s = %s\n", name, printed);
+  return result;
+}
+
 bool get_bool_knob(const char *name, bool default_value)
 {
-  return getenv(name) ? true : default_value;
+  return get_config(name) ? true : default_value;
 }
 
 bitmask_t get_bitmask_knob(const char *name, bitmask_t default_value)
 {
-  const char *str = getenv(name);
+  const char *str = get_config(name);
   assert(sizeof(bitmask_t) <= sizeof(long long));
   return str ? ((bitmask_t)strtoll(str, NULL, 0)) : default_value;
 }
 
 int get_int_knob(const char *name, int default_value) {
-  const char *str = getenv(name);
+  const char *str = get_config(name);
   return str ? ((int)strtol(str, NULL, 0)) : default_value;
 }
 
 const char *get_string_knob(const char *name, const char *default_value)
 {
-  const char *str = getenv(name);
+  const char *str = get_config(name);
   return str ? str : default_value;
 }
 
