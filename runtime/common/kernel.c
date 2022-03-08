f9dc46b4e5ee7dba6858dd7e62a2d44fd384e0b4
Anatoly Trosinenko <anatoly.trosinenko@gmail.com>
2022-03-08 16:41:13 +0300

    Factor out code for `mmap`ing huge pages if available

--- runtime/common/kernel.c
+++ runtime/common/kernel.c
@@ -101,6 +101,32 @@ static int mem_request(struct lkl_disk disk, struct lkl_blk_req *req)
   return LKL_DEV_BLK_STATUS_OK;
 }
 
+static void *map_internal(struct fuzzer_state *state, const char *desc, int fd, size_t size) {
+  void *result;
+  // First, try with HugeTLB enabled to speed up forkserver
+  TRACE_NO_NL(state, "Loading %s with HugeTLB... ", desc);
+  result = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB, -1, 0);
+  if (result != MAP_FAILED) {
+    TRACE(state, "OK");
+    if (fd >= 0)
+      CHECK_THAT(pread(fd, result, size, 0) == size);
+  } else {
+    WARN(state, "Cannot load %s with HugeTLB: %s", desc, strerror(errno));
+    TRACE_NO_NL(state, "Loading %s without HugeTLB... ", desc);
+    int flags = MAP_PRIVATE;
+    if (fd == -1)
+      flags |= MAP_ANONYMOUS;
+    result = mmap(NULL, size, PROT_READ | PROT_WRITE, flags, fd, 0);
+    if (result == MAP_FAILED) {
+      LOG_FATAL("Cannot load %s: %s", desc, strerror(errno));
+      abort();
+    } else {
+      TRACE(state, "OK");
+    }
+  }
+  return result;
+}
+
 void kernel_setup_disk(struct fuzzer_state *state, const char *filename, const char *fstype)
 {
   partition_t *partition = &state->partitions[state->constant_state.part_count];
@@ -121,24 +147,7 @@ void kernel_setup_disk(struct fuzzer_state *state, const char *filename, const c
 
   partition->size = size;
   strncpy(partition->fstype, fstype, sizeof(partition->fstype));
-
-  // First, try with HugeTLB enabled to speed up forkserver
-  TRACE_NO_NL(state, "Loading %s with HugeTLB... ", filename);
-  partition->data = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB, -1, 0);
-  if (partition->data != MAP_FAILED) {
-    TRACE(state, "OK");
-    CHECK_THAT(pread(image_fd, partition->data, size, 0) == size);
-  } else {
-    WARN(state, "Cannot load %s with HugeTLB: %s", filename, strerror(errno));
-    TRACE_NO_NL(state, "Loading %s without HugeTLB... ", filename);
-    partition->data = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE, image_fd, 0);
-    if (partition->data == MAP_FAILED) {
-      LOG_FATAL("Cannot load %s: %s", filename, strerror(errno));
-      abort();
-    } else {
-      TRACE(state, "OK");
-    }
-  }
+  partition->data = map_internal(state, filename, image_fd, size);
   close(image_fd);
 
   state->constant_state.part_count += 1;
