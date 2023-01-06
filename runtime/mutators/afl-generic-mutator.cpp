ff686a59224dba24c0cc04396a18275c0eb8e12d
Anatoly Trosinenko <anatoly.trosinenko@gmail.com>
2023-01-06 15:27:28 +0300

    Partially migrate afl-generic-mutator.c to C++

--- runtime/mutators/afl-generic-mutator.cpp
+++ runtime/mutators/afl-generic-mutator.cpp
@@ -7,6 +7,9 @@
 #include "kbdysch/hashing.h"
 #include "afl-interface-decls.h"
 
+#include <string>
+#include <vector>
+
 #include <assert.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -42,21 +45,42 @@ struct mutator_variable_desc {
   unsigned type;
   mutator_num_elements_t *num_elements;
   unsigned bytes_per_element, max_elements;
-  char *name;
+  std::string name;
   // Pointer to the original data in SHM
   void *data;
   // Pointer to the storage for data accumulated only for queue entries
   void *important_data;
+
+  bool fill_from_dereferenceable_shm(uint8_t *ptr);
 };
 
-struct mutator_shm {
+class mutator_shm {
+public:
+  mutator_shm(const char *env_var_name,
+              size_t allocatable_bytes, size_t total_bytes);
+
+  uint8_t *begin() { return shm_segment; }
+
+  bool in_bounds(uint8_t *ptr, size_t length) const;
+
+  ~mutator_shm();
+
+private:
   int shm_id;
   uint8_t *shm_segment;
   size_t allocatable_bytes;
 };
 
-struct mutator_temp_dir {
-#define MAX_DIR_NAME 128
+class mutator_temp_dir {
+public:
+  mutator_temp_dir(const char *name);
+
+  int fd() const { return dir_fd; }
+
+  ~mutator_temp_dir();
+
+private:
+  static const size_t MAX_DIR_NAME = 128;
   char dir_name[MAX_DIR_NAME];
   int dir_fd;
 };
@@ -71,13 +95,12 @@ struct harness_log {
 
 struct mutator_state {
   // Low-level details
-  struct mutator_shm variables_shm;
-  struct mutator_shm log_shm;
-  struct mutator_temp_dir log_dir;
+  mutator_shm *variables_shm;
+  mutator_shm *log_shm;
+  mutator_temp_dir *log_dir;
 
   // Variables area (parsed once per fuzzing session)
-  struct mutator_variable_desc variables[MUTATOR_MAX_VARIABLES];
-  int num_vars;
+  std::vector<mutator_variable_desc> variables;
   bool vars_shm_parsed;
 
   uint8_t input[MUTATOR_MAX_TEST_CASE_LENGTH];
@@ -113,37 +136,6 @@ static void init_error_logging(void) {
   setvbuf(error_log, NULL, _IONBF, 0);
 }
 
-static void init_shm(struct mutator_shm *shm, const char *env_var_name,
-                     size_t allocatable_bytes, size_t total_bytes) {
-  char buf[32];
-  // Allocate SHM segment
-  shm->shm_id = shmget(IPC_PRIVATE, total_bytes, 0600);
-  shm->shm_segment = shmat(shm->shm_id, NULL, 0);
-  shm->allocatable_bytes = allocatable_bytes;
-  // Make SHM discoverable by harness
-  sprintf(buf, "%d", shm->shm_id);
-  setenv(env_var_name, buf, 1);
-}
-
-static void drop_shm(struct mutator_shm *shm) {
-  shmdt(shm->shm_segment);
-  shmctl(shm->shm_id, IPC_RMID, NULL);
-}
-
-static void init_temp_dir(struct mutator_temp_dir *dir, const char *name) {
-  strncpy(dir->dir_name, name, MAX_DIR_NAME);
-  dir->dir_name[MAX_DIR_NAME - 1] = 0;
-  mkdtemp(dir->dir_name);
-
-  dir->dir_fd = open(dir->dir_name, O_RDONLY);
-  assert(dir->dir_fd >= 0);
-}
-
-static void drop_temp_dir(struct mutator_temp_dir *dir) {
-  close(dir->dir_fd);
-  // TODO remove directory
-}
-
 static bool in_bounds(const void *mem_area, size_t mem_size,
                       const void *ptr, size_t requested_size) {
   uintptr_t mem_start = (uintptr_t)mem_area;
@@ -165,71 +157,108 @@ static bool in_bounds(const void *mem_area, size_t mem_size,
   return true;
 }
 
+mutator_shm::mutator_shm(const char *env_var_name,
+                         size_t allocatable_bytes, size_t total_bytes)
+    : allocatable_bytes(allocatable_bytes) {
+  char buf[32];
+  // Allocate SHM segment
+  shm_id = shmget(IPC_PRIVATE, total_bytes, 0600);
+  shm_segment = (uint8_t *)shmat(shm_id, NULL, 0);
+  // Make SHM discoverable by harness
+  sprintf(buf, "%d", shm_id);
+  setenv(env_var_name, buf, 1);
+}
+
+bool mutator_shm::in_bounds(uint8_t *ptr, size_t length) const {
+  return ::in_bounds(shm_segment, allocatable_bytes,
+                     ptr, length);
+}
+
+mutator_shm::~mutator_shm() {
+  shmdt(shm_segment);
+  shmctl(shm_id, IPC_RMID, NULL);
+}
+
+mutator_temp_dir::mutator_temp_dir(const char *name) {
+  strncpy(dir_name, name, MAX_DIR_NAME);
+  dir_name[MAX_DIR_NAME - 1] = 0;
+  mkdtemp(dir_name);
+
+  dir_fd = open(dir_name, O_RDONLY);
+  assert(dir_fd >= 0);
+}
+
+mutator_temp_dir::~mutator_temp_dir() {
+  close(dir_fd);
+  // TODO remove directory
+}
+
+bool mutator_variable_desc::fill_from_dereferenceable_shm(uint8_t *ptr) {
+  DECL_WITH_TYPE(struct mutator_var_header, header, ptr);
+
+  type = header->type;
+  num_elements = &header->num_elements_real;
+  bytes_per_element = (unsigned)header->bytes_per_element;
+  max_elements = (unsigned)header->max_num_elements;
+
+  unsigned name_bytes = header->name_bytes; // Prevent sign-extension
+  name.assign((char *)&ptr[sizeof(*header)], name_bytes);
+
+  data = &ptr[sizeof(*header) + name_bytes];
+  DEBUG("  type = %d, size = %d x %d, name = %s\n",
+        type, bytes_per_element, max_elements, name);
+
+  switch (type) {
+  case MUTATOR_VAR_COUNTERS:
+    if (bytes_per_element != 8) {
+      DEBUG("Counter %s: unexpected bytes_per_element = %d.\n",
+            name, (int)bytes_per_element);
+      return false;
+    }
+    important_data = calloc(max_elements, bytes_per_element);
+    break;
+  case MUTATOR_VAR_STRINGS:
+    break;
+  case MUTATOR_VAR_STOP:
+    return false;
+  default:
+    DEBUG("Unknown var record type: 0x%x\n", (int)type);
+    return false;
+  }
+  return true;
+}
+
 static void parse_variables_area(struct mutator_state *state) {
   if (state->vars_shm_parsed)
     return;
   state->vars_shm_parsed = true;
 
-  struct mutator_shm *shm = &state->variables_shm;
-  uint8_t *current_ptr = shm->shm_segment;
+  mutator_shm *shm = state->variables_shm;
+  uint8_t *current_ptr = shm->begin();
   for (int current_var = 0; ; ++current_var) {
     DEBUG("Parsing variable #%d...\n", current_var);
-    if (current_var >= MUTATOR_MAX_VARIABLES) {
-      DEBUG("Too many variables (max %d supported).\n", MUTATOR_MAX_VARIABLES);
-      return;
-    }
-    struct mutator_variable_desc *desc = &state->variables[current_var];
 
     // Check input description is in bounds
     size_t total_bytes = sizeof(struct mutator_var_header);
-    if (!in_bounds(shm->shm_segment, shm->allocatable_bytes, current_ptr, total_bytes))
+    if (!shm->in_bounds(current_ptr, total_bytes))
       return;
     DECL_WITH_TYPE(struct mutator_var_header, header, current_ptr);
-    total_bytes += header->name_bytes;
-    total_bytes += header->bytes_per_element * header->max_num_elements;
-    if (!in_bounds(shm->shm_segment, shm->allocatable_bytes, current_ptr, total_bytes))
+    total_bytes += (unsigned)header->name_bytes;
+    total_bytes += (unsigned)header->bytes_per_element * (unsigned)header->max_num_elements;
+    if (!shm->in_bounds(current_ptr, total_bytes))
       return;
 
-    // Parse input description
-    desc->type = header->type;
-    desc->num_elements = &header->num_elements_real;
-    desc->bytes_per_element = (unsigned)header->bytes_per_element;
-    desc->max_elements = (unsigned)header->max_num_elements;
-
-    unsigned name_bytes = header->name_bytes; // Prevent sign-extension
-    desc->name = malloc(name_bytes + 1);
-    memcpy(desc->name, &current_ptr[sizeof(*header)], name_bytes);
-    desc->name[name_bytes] = '\0';
-
-    desc->data = &current_ptr[sizeof(*header) + name_bytes];
-    DEBUG("  type = %d, size = %d x %d, name = %s\n",
-          desc->type, desc->bytes_per_element, desc->max_elements, desc->name);
-
+    mutator_variable_desc desc;
+    if (!desc.fill_from_dereferenceable_shm(current_ptr))
+      return;
+    state->variables.push_back(desc);
     current_ptr += total_bytes;
-    switch (desc->type) {
-    case MUTATOR_VAR_COUNTERS:
-      if (desc->bytes_per_element != 8) {
-        DEBUG("Counter %s: unexpected bytes_per_element = %d.\n",
-              desc->name, (int)desc->bytes_per_element);
-        return;
-      }
-      desc->important_data = calloc(desc->max_elements, desc->bytes_per_element);
-      break;
-    case MUTATOR_VAR_STRINGS:
-      break;
-    case MUTATOR_VAR_STOP:
-      state->num_vars = current_var;
-      return;
-    default:
-      DEBUG("Unknown var record type: 0x%x\n", (int)desc->type);
-      return;
-    }
   }
 }
 
 static void accumulate_important_data(struct mutator_state *state) {
   parse_variables_area(state);
-  for (int current_var = 0; current_var < state->num_vars; ++current_var) {
+  for (int current_var = 0; current_var < state->variables.size(); ++current_var) {
     struct mutator_variable_desc *v = &state->variables[current_var];
     switch (v->type) {
     case MUTATOR_VAR_COUNTERS: {
@@ -248,13 +277,13 @@ static void accumulate_important_data(struct mutator_state *state) {
 static void print_scalar_variable(struct mutator_variable_desc *desc, int index) {
   switch (desc->type) {
   case MUTATOR_VAR_COUNTERS: {
-    mutator_u64_var_t *counters = desc->data;
-    mutator_u64_var_t *important = desc->important_data;
+    mutator_u64_var_t *counters = (mutator_u64_var_t *)desc->data;
+    mutator_u64_var_t *important = (mutator_u64_var_t *)desc->important_data;
     printf("%lu\t%lu", (unsigned long)important[index], (unsigned long)counters[index]);
     break;
   }
   case MUTATOR_VAR_STRINGS: {
-    char *strings = desc->data;
+    char *strings = (char *)desc->data;
     char *str = &strings[index * desc->bytes_per_element];
     char *str_end = &str[desc->bytes_per_element];
     char tmp = *str_end;
@@ -270,7 +299,7 @@ static void print_scalar_variable(struct mutator_variable_desc *desc, int index)
 }
 
 static void print_variables(struct mutator_state *state) {
-  for (int current_var = 0; current_var < state->num_vars; ++current_var) {
+  for (int current_var = 0; current_var < state->variables.size(); ++current_var) {
     struct mutator_variable_desc *desc = &state->variables[current_var];
     unsigned num_elements = *desc->num_elements;
     if (num_elements > desc->max_elements) {
@@ -281,9 +310,9 @@ static void print_variables(struct mutator_state *state) {
     if (num_elements == 1) {
       printf("Variable #%u:\t", current_var);
       print_scalar_variable(desc, 0);
-      printf("\t - %s\n", desc->name);
+      printf("\t - %s\n", desc->name.c_str());
     } else {
-      printf("Variable #%u: %s\n", current_var, desc->name);
+      printf("Variable #%u: %s\n", current_var, desc->name.c_str());
       for (unsigned i = 0; i < num_elements; ++i) {
         printf("- [%u] = ", i);
         print_scalar_variable(desc, i);
@@ -315,7 +344,7 @@ static bool load_log(struct mutator_state *state,
   kbdysch_compute_hash(hash, buf, buf_length);
   hash[HASH_CHARS] = '\0';
 
-  int log_fd = openat(state->log_dir.dir_fd, hash, O_RDONLY);
+  int log_fd = openat(state->log_dir->fd(), hash, O_RDONLY);
   if (log_fd < 0) {
     ERR("Cannot open log for hash %s\n", hash);
     memset(log, 0, sizeof(*log));
@@ -463,7 +492,7 @@ static void render_splice(struct mutator_state *state,
   if (!load_log(state, &state->additional_log, add_buf, add_buf_size)) {
     memcpy(state->output, add_buf, add_buf_size);
     state->output_length = add_buf_size;
-    state->record_log_next_action = RECORD_LOG_CAPTURE_LOG_AS_IS;
+    state->record_log_next_action = mutator_state::RECORD_LOG_CAPTURE_LOG_AS_IS;
     return;
   }
   if (!state->additional_log.num_offsets)
@@ -503,7 +532,7 @@ static bool save_log_from_shm(struct mutator_state *state,
   if (length == 0) {
     assert(test_case == NULL);
     // Don't compare the hash, just save what we got
-    memcpy(hash, state->log_shm.shm_segment, HASH_CHARS);
+    memcpy(hash, state->log_shm->begin(), HASH_CHARS);
     hash[HASH_CHARS] = '\0';
     if (strspn(hash, "0123456789abcdef") != HASH_CHARS) {
       ERR("Unexpected characters in hash\n");
@@ -512,9 +541,9 @@ static bool save_log_from_shm(struct mutator_state *state,
   } else {
     kbdysch_compute_hash(hash, test_case, length);
     hash[HASH_CHARS] = '\0';
-    if (memcmp(state->log_shm.shm_segment, hash, HASH_CHARS)) {
+    if (memcmp(state->log_shm->begin(), hash, HASH_CHARS)) {
       char real_hash[HASH_CHARS + 1];
-      memcpy(real_hash, state->log_shm.shm_segment, HASH_CHARS);
+      memcpy(real_hash, state->log_shm->begin(), HASH_CHARS);
       real_hash[HASH_CHARS] = 0;
       ERR("Hash mismatch:  %s  (%s)\n", hash, description);
       ERR("         Real: [%s]\n", real_hash);
@@ -522,10 +551,10 @@ static bool save_log_from_shm(struct mutator_state *state,
     }
   }
 
-  int log_fd = openat(state->log_dir.dir_fd, hash, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+  int log_fd = openat(state->log_dir->fd(), hash, O_WRONLY | O_CREAT | O_TRUNC, 0644);
   if (log_fd < 0)
     FATAL("Cannot write log data: %s.\n", strerror(errno));
-  write(log_fd, &state->log_shm.shm_segment[HASH_CHARS], MUTATOR_MAX_LOG_BYTES);
+  write(log_fd, &state->log_shm->begin()[HASH_CHARS], MUTATOR_MAX_LOG_BYTES);
   close(log_fd);
 
   return true;
@@ -538,12 +567,14 @@ void *afl_custom_init(/*afl_state_t*/ void *afl_, unsigned int seed_) {
 
   init_error_logging();
 
-  struct mutator_state *state = calloc(1, sizeof(*state));
-  init_shm(&state->variables_shm, MUTATOR_SHM_VARS_ENV_NAME,
-           MUTATOR_SHM_VARS_BYTES, 2 * MUTATOR_SHM_VARS_BYTES);
-  init_shm(&state->log_shm, MUTATOR_SHM_LOG_ENV_NAME,
-           MUTATOR_SHM_LOG_BYTES, MUTATOR_SHM_LOG_BYTES);
-  init_temp_dir(&state->log_dir, "/tmp/afl-mutator-XXXXXX");
+  struct mutator_state *state = (struct mutator_state *)calloc(1, sizeof(*state));
+  state->variables_shm = new mutator_shm(
+    MUTATOR_SHM_VARS_ENV_NAME,
+    MUTATOR_SHM_VARS_BYTES, 2 * MUTATOR_SHM_VARS_BYTES);
+  state->log_shm = new mutator_shm(
+    MUTATOR_SHM_LOG_ENV_NAME,
+    MUTATOR_SHM_LOG_BYTES, MUTATOR_SHM_LOG_BYTES);
+  state->log_dir = new mutator_temp_dir("/tmp/afl-mutator-XXXXXX");
 
   return state;
 }
@@ -557,7 +588,7 @@ uint32_t afl_custom_fuzz_count(void *data, const uint8_t *buf, size_t buf_size) 
   state->current_mutation = 0;
 
   state->best_effort_mode = !has_log;
-  state->record_log_next_action = has_log ? RECORD_LOG_NONE : RECORD_LOG_PREPARE_INPUT;
+  state->record_log_next_action = has_log ? mutator_state::RECORD_LOG_NONE : mutator_state::RECORD_LOG_PREPARE_INPUT;
 
   if (state->best_effort_mode) {
     ERR("  %zu bytes, recording log...\n", buf_size);
@@ -576,29 +607,29 @@ size_t afl_custom_fuzz(void *data, uint8_t *buf, size_t buf_size, uint8_t **out_
   DEBUG_TRACE_FUNC;
 
   switch (state->record_log_next_action) {
-  case RECORD_LOG_NONE:
+  case mutator_state::RECORD_LOG_NONE:
     break;
-  case RECORD_LOG_PREPARE_INPUT:
+  case mutator_state::RECORD_LOG_PREPARE_INPUT:
     ERR("Requesting log...\n");
     if (state->input_length != buf_size || memcmp(state->input, buf, state->input_length))
       ERR("Unexpected buf and buf_size=%zu\n", buf_size);
     ++state->current_mutation;
-    state->record_log_next_action = RECORD_LOG_CAPTURE_LOG;
+    state->record_log_next_action = mutator_state::RECORD_LOG_CAPTURE_LOG;
     *out_buf = buf;
     return buf_size;
-  case RECORD_LOG_CAPTURE_LOG:
+  case mutator_state::RECORD_LOG_CAPTURE_LOG:
     ERR("Trying to save log...\n");
     if (!save_log_from_shm(state, state->input, state->input_length, "best effort mode"))
       abort();
     accumulate_important_data(state);
     load_log(state, &state->current_log, state->input, state->input_length);
-    state->record_log_next_action = RECORD_LOG_NONE;
-  case RECORD_LOG_CAPTURE_LOG_AS_IS:
+    state->record_log_next_action = mutator_state::RECORD_LOG_NONE;
+  case mutator_state::RECORD_LOG_CAPTURE_LOG_AS_IS:
     ERR("Trying to save log as-is...\n");
     if (!save_log_from_shm(state, NULL, 0, "best effort mode"))
       abort();
     accumulate_important_data(state);
-    state->record_log_next_action = RECORD_LOG_NONE;
+    state->record_log_next_action = mutator_state::RECORD_LOG_NONE;
     break;
   }
 
@@ -662,13 +693,10 @@ void afl_custom_deinit(void *data) {
   parse_variables_area(state);
   print_variables(state);
 
-  for (int i = 0; i < state->num_vars; ++i) {
-    free(state->variables[i].name);
-  }
-  drop_temp_dir(&state->log_dir);
-  drop_shm(&state->log_shm);
-  drop_shm(&state->variables_shm);
-  free(data);
+  delete state->log_dir;
+  delete state->log_shm;
+  delete state->variables_shm;
+  delete state;
 
   if (error_log)
     fclose(error_log);
