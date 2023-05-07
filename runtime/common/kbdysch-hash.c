3a5248ee2fad37e5fd3757fc38e3ae525b74e8bd
Anatoly Trosinenko <anatoly.trosinenko@gmail.com>
2023-05-07 18:45:09 +0300

    Add a trivial wrapper for kbdysch_compute_hash()

---
+++ runtime/common/kbdysch-hash.c
@@ -0,0 +1,15 @@
+#include "kbdysch/hashing.h"
+
+#include <stdio.h>
+#include <unistd.h>
+
+uint8_t buf[4096];
+
+int main() {
+  char hash[HASH_CHARS + 1];
+  int length = read(0, buf, sizeof(buf));
+  kbdysch_compute_hash(hash, buf, length);
+  hash[HASH_CHARS] = 0;
+  printf("%s\n", hash);
+  return 0;
+}
